14729c970ee57dd14d4cd19fe5b2d8eb38a34772
Gabriel Marinho <gabriel.marinho.silva@outlook.com>
2025-07-18 01:33:09 -0300

    DaoFactory and sellerDaoJDBC

---
+++ src/main/java/model/dao/DaoFactory.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    /*
+        É uma forma da minha classe expor um método que retorna o tipo da interface, mas internamente ela está
+        instanciando uma implementacao. É uma forma de não expor a implementação, mostrando apenas a interface, ou seja,
+        o programa não conhece a implementação, apenas a interface
+     */
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+}
